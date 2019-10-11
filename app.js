f91ca4a7054f008216f77c4e9493b6513c086ac5
Vince Lollino <charlie_fog@comcast.net>
2019-10-10 19:59:57 -0500

    Added require Mongoose

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 var config = require('./config.dev');
 
-
+var mongoose = require('mongoose');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,12 +27,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -40,6 +40,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+//Connect to MongoDB
+  mongoose.connect(config.mongodb, {
+  useNewUrlParser: true
 });
 
 module.exports = app;
