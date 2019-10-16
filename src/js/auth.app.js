2399ff77fa01bd4dd1990610f4eb9ec3058931f8
Vince Lollino <charlie_fog@comcast.net>
2019-10-15 21:24:10 -0500

    Added an XHR post request method

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -1,9 +1,9 @@
-var authApp = (function() {
+var authApp = (function () {
 
-  function loginForm(){
+  function loginForm() {
     let app = document.getElementById('app');
 
-    let form =  `
+    let form = `
       <div class="card login-form">
         <form id="loginForm" class="card-body">
           <h1 class="card-title text-center">Please Sign In</h1>
@@ -23,15 +23,46 @@ var authApp = (function() {
       </div>
     `;
 
-    app.innerHTML=form;
+    app.innerHTML = form;
+  }
+
+  function postRequest(formId, url) {
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function (value, key) {
+        object[key] = value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function () {
+        let data = JSON.parse(xhr.response);
+        console.log(data);
+      }
+    });
   }
 
   return {
-    load: function(){
+    load: function () {
       loginForm();
+      postRequest('loginForm', '/api/auth/login');
     }
   }
 
+
 })();
+
 
 authApp.load();
