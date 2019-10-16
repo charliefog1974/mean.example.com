0b193624cc720762dbe420dcfb7f033f521314d8
Vince Lollino <charlie_fog@comcast.net>
2019-10-16 10:53:39 -0500

    changed some things hope they work

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -57,7 +57,15 @@ router.post('/login', function (req, res, next) {
 
             //we will use a console.log() to test the session data
             console.log(req.session);
-
+           
+            router.delete('/logout', function(req, res){
+                req.logout();
+                if(!req.session.passport.user){
+                  return res.json({success: 'true'});
+                }else{
+                  return res.json({success: 'false'});
+                }
+              });
             return res.json({ success: true, user: user });
 
         });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) { console.log(req);
-  res.render('index', { title: 'Express', name: 'Vinnie' });
+router.get('/', function(req, res, next) {
+  console.log(req.session);
+  res.render('index', { title: 'Express' });
 });
+
+// router.get('/logout', function(req, res){
+//   console.log(req.session);
+//   req.logout();
+//   console.log(req.session);
+// });
 
 module.exports = router;
