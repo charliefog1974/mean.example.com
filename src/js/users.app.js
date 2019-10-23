55c46ba0c88c6a048546f34c8168ac6744a52b28
Vince Lollino <charlie_fog@comcast.net>
2019-10-22 20:37:08 -0500

    Created method for deleting user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -280,8 +280,8 @@ var usersApp = (function () {
               
                       <div class="text-center">
                         <br>
-                        <a class="btn btn-lg btn-danger text-white">
-                          Yes delete ${data.user.username}
+                        <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+                            Yes delete ${data.user.username}
                         </a>
                       </div>
               
@@ -292,6 +292,31 @@ var usersApp = (function () {
                 }
             }
 
+
+            function deleteUser(id) {
+
+                let uri = `${window.location.origin}/api/users/${id}`;
+                let xhr = new XMLHttpRequest();
+                xhr.open('DELETE', uri);
+
+                xhr.setRequestHeader(
+                    'Content-Type',
+                    'application/json; charset=UTF-8'
+                );
+
+                xhr.send();
+
+                xhr.onload = function () {
+                    let data = JSON.parse(xhr.response);
+                    if (data.success === true) {
+                        window.location.hash = '#';
+                    } else {
+                        alert('Unknown error, the user could not be deleted');
+                    }
+
+                }
+
+            }
         });
     }
 
@@ -338,6 +363,10 @@ var usersApp = (function () {
                     viewUsers();
                     break;
             }
+        },
+
+        deleteUser: function (id) {
+            deleteUser(id);
         }
     }
 
